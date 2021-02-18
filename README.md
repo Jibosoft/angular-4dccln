6e4152b347f9601308c5b9b60d36100bac6435ff
Jibosoft <shuaibujibril54@gmail.com>
2021-02-18 15:54:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-4dccln
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-4dccln)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'ABAYA',
+    price: 799,
+    description: 'A gown worn by female most expecialy ARABS'
+  },
+  {
+    id: 2,
+    name: 'PERFUME',
+    price: 699,
+    description: 'A Frangrance worn by everybody'
+  },
+  {
+    id: 3,
+    name: 'Gorontula',
+    price: 299,
+    description: 'Herbal Medical fruit for curing infertility'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
